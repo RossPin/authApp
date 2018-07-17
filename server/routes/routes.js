db1fc84b9884537ceb3a1b11ce1d4c96ae8adee1
RossPin <dorpetz@hotmail.com>
2018-07-17 14:02:48 +1200

    getting react setup

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,12 +2,6 @@ const express = require('express')
 const router = express.Router()
 const {getUsers} = require('../db/users')
 
-router.get('/', (req, res) => {
-    getUsers().then(users => {
-        console.log(users)
-        res.send('Hello')
-    })   
-    
-})
+
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const express = require('express')
 const server = express()
 const routes = require('./routes/routes.js')
 const auth = require('./routes/auth.js')
+const path = require('path')
 
 server.use('/', routes)
+
+server.use(express.static(path.join(__dirname, '../public')))
 
 server.use(express.json())
 server.use('/api/auth', auth)
